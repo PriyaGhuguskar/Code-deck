2dbc6698415fc7ef9808432a031a639c90513bc6
priyaghuguskar <ghuguskarpriya@gmail.com>
2023-03-06 15:49:57 +0530

    loading change

--- src/Component/ModalTypes/Loading.js
+++ src/Component/ModalTypes/Loading.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import loadinGif from './gif.gif'
 
 function Loading() {
+ 
+
   return (
-    <div className="px-4 py-4">Loading.....
+    <div className="px-4 py-4 h-24 flex items-center">
+    <span> Loading..... </span>
+    <span><img class="h-24" src={loadinGif} alt=""/> </span>
     {/* <iframe src="https://embed.lottiefiles.com/animation/100199" className="px-4 py-4 w-40"></iframe> */}
     </div>
   )
